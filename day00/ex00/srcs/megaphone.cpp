12b04f8c09b8ee27ef4373a6f0de135f69bf9ff2
Anne-laure VACHER <avacher@e3r3p4.42.fr>
2018-03-26 22:11:10 +0200

    piscine C++ - day00 -- end day

---
+++ day00/ex00/srcs/megaphone.cpp
@@ -0,0 +1,24 @@
+#include <cctype>
+#include <iostream>
+#include <locale>
+
+int main(int ac, char **av)
+{
+    std::locale loc;
+    std::string upper;
+
+    if (ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        for (int i = 1; i < ac; ++i)
+        {
+            upper = av[i];
+            for (std::string::size_type j = 0; j < upper.length(); ++j)
+                std::cout << std::toupper(upper[j], loc);
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
---
+++ day00/ex02/srcs/Account.class.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <chrono>
+#include "Account.class.hpp"
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+int Account::getNbAccounts(void)
+{
+    return Account::_nbAccounts;
+}
+
+int Account::getTotalAmount(void)
+{
+    return Account::_totalAmount;
+}
+
+int Account::getNbDeposits(void)
+{
+    return Account::_totalNbDeposits;
+}
+
+int Account::getNbWithdrawals(void)
+{
+    return Account::_totalNbWithdrawals;
+}
+
+void Account::displayAccountsInfos(void)
+{
+    Account::_displayTimestamp();
+    std::cout << "accounts:" << Account::_nbAccounts;
+    std::cout << ";total:" << Account::_totalAmount;
+    std::cout << ";deposits:" << Account::_totalNbDeposits;
+    std::cout << ";withdrawals:" << Account::_totalNbWithdrawals << std::endl;
+}
+
+Account::Account(int initial_deposit)
+{
+    this->_accountIndex = Account::_nbAccounts;
+    Account::_nbAccounts++;
+    this->_amount = initial_deposit;
+    Account::_totalAmount += this->_amount;
+
+    this->_nbDeposits = 0;
+    this->_nbWithdrawals = 0;
+
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex;
+    std::cout << ";amount:" << this->_amount << ";created" << std::endl;
+}
+
+Account::~Account(void)
+{
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex;
+    std::cout << ";amount:" << this->_amount << ";closed" << std::endl;
+}
+
+void Account::makeDeposit(int deposit)
+{
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex;
+    std::cout << ";p_amount:" << this->_amount;
+    std::cout << ";deposits:" << deposit;
+
+    this->_amount += deposit;
+    Account::_totalAmount += deposit;
+    this->_nbDeposits++;
+    Account::_totalNbDeposits++;
+
+    std::cout << ";amount:" << this->_amount;
+    std::cout << ";nb_deposits:" << this->_nbDeposits << std::endl;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex;
+    std::cout << ";p_amount:" << this->_amount;
+    std::cout << ";withdrawal:";
+
+    if (this->_amount - withdrawal < 0)
+    {
+        std::cout << "refused" << std::endl;
+        return false;
+    }
+
+    std::cout << withdrawal;
+
+    this->_amount -= withdrawal;
+    Account::_totalAmount -= withdrawal;
+    this->_nbWithdrawals++;
+    Account::_totalNbWithdrawals++;
+
+    std::cout << ";amount:" << this->_amount;
+    std::cout << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+    return true;
+}
+
+int Account::checkAmount(void) const
+{
+    return 0;
+}
+
+void Account::displayStatus(void) const
+{
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex;
+    std::cout << ";amount:" << this->_amount;
+    std::cout << ";deposits:" << this->_nbDeposits;
+    std::cout << ";withdrawals:" << this->_nbWithdrawals << std::endl;
+}
+
+void Account::_displayTimestamp(void)
+{
+    std::time_t tt = std::chrono::system_clock::system_clock::to_time_t(std::chrono::system_clock::system_clock::now());
+
+    struct std::tm *ptm = std::localtime(&tt);
+    std::cout << "[" << 1900 + ptm->tm_year << 1 + ptm->tm_mon << ptm->tm_mday << "_";
+    std::cout << ptm->tm_hour << ptm->tm_min << ptm->tm_sec << "] ";
+}
